17e24c3124993aab7292cbda7478c837c358fc86
Tarek Ait Ahmed <nt_ait_ahmed@esi.dz>
2024-07-25 01:07:25 +0200

    adding my touch

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <NavBar></NavBar>
   <router-view></router-view>
+  <h1 style="font-size: 15px;margin-top: 3rem;margin-bottom: 3rem;font-weight: 600;text-align: center;">Made with ❤️ by Tarek Ait Ahmed</h1>
 </template>
 
 
